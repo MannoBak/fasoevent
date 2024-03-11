99bd687c353ced2574ea6f462de4dde18591d0e1
hermannbakouan51@gmail.com <MannoBak>
2024-03-11 14:50:04 +0000

    Initialisation du projet

---
+++ resources/views/public/_layout/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav id="navbar" class="navbar">
+          <ul>
+            <li>
+              <a class="nav-link scrollto active" href="#hero">Accueil</a>
+            </li>
+            <li><a class="nav-link scrollto" href="#about">A propos</a></li>
+            <li>
+              <a class="nav-link scrollto" href="#portfolio">Portfolio</a>
+            </li>
+            <li><a class="nav-link scrollto" href="#team">Equipe</a></li>
+            <li><a href="{{ route('public.evenements') }}">Evénément</a></li>
+            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\public\AccueilController;
+use App\Http\Controllers\public\EvenementController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AccueilController::class, 'index']) ->name('accueil');
+Route::get('/evenements', [AccueilController::class, 'index']) ->name('public.evenements');
+
